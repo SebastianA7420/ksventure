79cc569469b2b6b5688636f1981227c72803f3a1
Ibrahim Amin <ibrahimamin@Ibrahims-Air.fios-router.home>
2018-08-08 06:04:13 -0400

    getting the camera to work

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,14 @@
 var vue = new Vue ({
-                el: '#app',
-                data: {
-                    message: 'click here', page: ''
-                }
-            });
+    el: '#app',
+    data: {
+        message: 'click here', page: ''
+    },
+    updated: function() {
+      if (window.vueControllers[vue.page]) {
+        window.vueControllers[vue.page]();
+      }
+    }
+});
+
+// This is where controller code will live for each view
+window.vueControllers = {};
