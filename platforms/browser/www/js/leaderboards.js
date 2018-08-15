0c4e8583597f8b4d7b14cff62210b8a06b720c06
sebastian <sebastiana7420@hstat.org>
2018-08-15 01:46:09 -0400

    work on leaderboard and add pics

--- platforms/browser/www/js/leaderboards.js
+++ platforms/browser/www/js/leaderboards.js
@@ -2,8 +2,7 @@ vue.leaderboards = `
 <div style="font-family: Montserrat; padding-top: 75px;">
   <ul v-hide="vue.showEntries" class="table-view">
     <li class="table-view-cell media">
-      <a class="navigate-right">
-       <img class="media-object pull-left" src="http://placehold.it/100x100">
+       <img class="media-object pull-left" src="VentureBoy.jpg">
              <div class="media-body">
           #1
           <h4>blah blah</h4>
@@ -11,8 +10,7 @@ vue.leaderboards = `
       </a>
     </li>
     <li class="table-view-cell media">
-      <a class="navigate-right">
-        <img class="media-object pull-left" src="http://placehold.it/100x100">
+        <img class="media-object pull-left" src="VentureGirl.jpg">
         <div class="media-body">
           #2
           <h4>blah blah</h4>
@@ -20,8 +18,7 @@ vue.leaderboards = `
       </a>
     </li>
     <li class="table-view-cell media">
-      <a class="navigate-right">
-        <img class="media-object pull-left" src="http://placehold.it/100x100">
+        <img class="media-object pull-left" src="VentureBoy2.jpg">
         <div class="media-body">
           #3
           <h4>blah blah</h4>
