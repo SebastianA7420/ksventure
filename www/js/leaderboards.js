cf7a1ec696a6ac9dd9f606aa5d28d08c15a0ea9d
Seb <SebastianA7420@hstat.org>
2018-08-15 18:53:21 -0400

    restore leadboard files

--- www/js/leaderboards.js
+++ www/js/leaderboards.js
@@ -1,27 +1,30 @@
 vue.leaderboards = `
+<div style="background:linear-gradient(to bottom right, #e6a3ff, #b0c5fc);">
+<br>
+<h2>Leaderboard</h2>
 <div style="font-family: Montserrat; padding-top: 75px;">
   <ul v-hide="vue.showEntries" class="table-view">
     <li class="table-view-cell media">
-       <img class="media-object pull-left" src="VentureBoy.jpg">
+       <img class="media-object pull-left" src="VentureBoy.jpg" width="250" height="250">
              <div class="media-body">
-          #1
-          <h4>blah blah</h4>
+          <h3>Brian Tan<h/3>
+          <h4>First Place Venturer</h4>
         </div>
       </a>
     </li>
     <li class="table-view-cell media">
-        <img class="media-object pull-left" src="VentureGirl.jpg">
+        <img class="media-object pull-left" src="VentureGirl.jpg" width="250" height="250">
         <div class="media-body">
-          #2
-          <h4>blah blah</h4>
+          <h3>Jasmine Miller<h/3>
+          <h4>Second Place Venturer</h4>
         </div>
       </a>
     </li>
     <li class="table-view-cell media">
-        <img class="media-object pull-left" src="VentureBoy2.jpg">
+        <img class="media-object pull-left" src="VentureBoy2.jpg" width="250" height="250">
         <div class="media-body">
-          #3
-          <h4>blah blah</h4>
+          <h3>Devon Gabriel<h/3>
+          <h4>Third Place Venturer</h4>
         </div>
       </a>
     </li>
