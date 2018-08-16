25d4d4e489264e9799196cd64d704c921cd9ac36
Seb <SebastianA7420@hstat.org>
2018-08-16 17:25:48 -0400

    work on leaderboard pt 3

--- platforms/android/app/src/main/assets/www/js/feed.js
+++ platforms/android/app/src/main/assets/www/js/feed.js
@@ -1,9 +1,10 @@
 
 vue.feed = `
 
+
 <header class="bar bar-nav" style="font-size:50px; font-family: Oswald; text-shadow: 5px 5px #b0c5fc;">
   <img id="venture" src="splash.png" style="width:15%; float:left; padding: 17px;">
-  <h1 class="title">Venture</h1>
+
 </header>
 <ul class="table-view">
   <br><br><br><br><br>
