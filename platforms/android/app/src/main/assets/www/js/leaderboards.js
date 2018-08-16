24bb088759f314703ff1c98c279cffeb60b1491f
ksagar0052 <ksagar0052@bths.edu>
2018-08-15 20:41:43 -0400

    feed stuff

--- platforms/android/app/src/main/assets/www/js/leaderboards.js
+++ platforms/android/app/src/main/assets/www/js/leaderboards.js
@@ -1,33 +1,55 @@
 vue.leaderboards = `
 <div style="background:linear-gradient(to bottom right, #e6a3ff, #b0c5fc);">
+<<<<<<< HEAD
 <h2><center>Leaderboards</center></h2>
 <center><img src="VentureTrophy.png" alt="VENTURE TROPHY" width="100" height="100"></center>
+=======
+<br>
+<h2>Leaderboard</h2>
+>>>>>>> 10f058e... feed stuff
 <div style="font-family: Montserrat; padding-top: 75px;">
   <ul v-hide="vue.showEntries" class="table-view">
     <li class="table-view-cell media">
        <img class="media-object pull-left" src="VentureBoy.jpg" width="250" height="250">
              <div class="media-body">
+<<<<<<< HEAD
           <h3>Brian T.<h/3>
+=======
+          <h3>Brian Tan<h/3>
+>>>>>>> 10f058e... feed stuff
           <h4>First Place Venturer</h4>
         </div>
       </a>
     </li>
     <li class="table-view-cell media">
+<<<<<<< HEAD
         <img class="media-object pull-left" src="VentureBoy2.png" width="250" height="250">
         <div class="media-body">
           <h3>Devon H.<h/3>
+=======
+        <img class="media-object pull-left" src="VentureGirl.jpg" width="250" height="250">
+        <div class="media-body">
+          <h3>Jasmine Miller<h/3>
+>>>>>>> 10f058e... feed stuff
           <h4>Second Place Venturer</h4>
         </div>
       </a>
     </li>
     <li class="table-view-cell media">
+<<<<<<< HEAD
         <img class="media-object pull-left" src="VentureGirl2.png" width="250" height="250">
         <div class="media-body">
           <h3>Jasmine M.<h/3>
+=======
+        <img class="media-object pull-left" src="VentureBoy2.jpg" width="250" height="250">
+        <div class="media-body">
+          <h3>Devon Gabriel<h/3>
+>>>>>>> 10f058e... feed stuff
           <h4>Third Place Venturer</h4>
         </div>
       </a>
     </li>
+<<<<<<< HEAD
     <li class="table-view-cell media">
        <img class="media-object pull-left" src="VentureBoy3.png" width="250" height="250">
              <div class="media-body">
@@ -48,3 +70,8 @@ vue.leaderboards = `
 </div>
 `
 
+=======
+  </ul>
+</div>
+`
+>>>>>>> 10f058e... feed stuff
--- www/js/feed.js
+++ www/js/feed.js
@@ -1,16 +1,24 @@
 
 vue.feed = `
-<div id="backgnd">
+
+<header class="bar bar-nav" style="font-size:50px; font-family: Squimpy;
+    text-shadow: 5px 5px #b0c5fc;">
+  <img id="venture" src="splash.png" style="width:10%; float:left;">
+  <h1 class="title">Venture</h1>
+</header>
 <ul class="table-view">
-  <br><br><br><br>
-  <h1 class="catchphrase">Feed</h2>
+  <br><br><br>
   <li class="table-view-cell media">
     <a class="navigate-right">
-      <img class="media-object pull-left" src="outfit.jpg">
+      <img class="media-object pull-left" src="outfit.jpg" >
       <div class="media-body">
-        Strangers Choose My Outfit at Primark!
+        <p style="font-size: 28px">Strangers Choose My Outfit at Primark!</p>
         <p>By Samantha Somebody</p>
         <p>Check out my fun adventure!</p>
+        <p style="font-size: 20px">Rate this adventure!</p>
+		<div class="rating">
+		<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+		</div>
       </div>
     </a>
   </li>
@@ -18,9 +26,13 @@ vue.feed = `
     <a class="navigate-right">
       <img class="media-object pull-left" src="museum.jpg">
       <div class="media-body">
-        Look What I Found at the Museum of Natural History!
+        <p style="font-size: 28px">Look What I Found at the Museum of Natural History!</p>
         <p>By Joe Bob</p>
         <p>It took me so long to find this statue, but it was so worth the wait...</p>
+        <p style="font-size: 20px">Rate this adventure!</p>
+		<div class="rating">
+		<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+		</div>
       </div>
     </a>
   </li>
@@ -28,9 +40,13 @@ vue.feed = `
     <a class="navigate-right">
       <img class="media-object pull-left" src="bridge.jpg">
       <div class="media-body">
-        Running Through the Brooklyn Bridge!
+        <p style="font-size: 28px">Running Through the Brooklyn Bridge!</p>
         <p>By RyanChris Blah</p>
         <p>I made a new friend while I was running the bridge! And I feel so active ;)</p>
+        <p style="font-size: 20px">Rate this adventure!</p>
+		<div class="rating">
+		<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+		</div>
       </div>
     </a>
   </li>
@@ -38,12 +54,15 @@ vue.feed = `
     <a class="navigate-right">
       <img class="media-object pull-left" src="DanceChallenge.jpg">
       <div class="media-body">
-        Choreographing My Own Dance at Times Square!
+        <p style="font-size: 28px">Choreographing My Own Dance at Times Square!</p>
         <p>By John DaBest</p>
         <p>I've never really danced before, but I got a few people to help me create my own cool little routine! Check it out.</p>
+        <p style="font-size: 20px">Rate this adventure!</p>
+		<div class="rating">
+		<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+		</div>
       </div>
     </a>
   </li>
 </ul>
-</div>
 `
